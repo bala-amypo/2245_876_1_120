332db2e54544441c848fe13fa4853ef0d1156574
bala-amypo <bala@amypo.in>
2025-12-19 05:34:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/KeyExemption.java
+++ src/main/java/com/example/demo/entity/KeyExemption.java
@@ -1,27 +1,27 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
+import lombok.*;
+
+import java.time.Instant;
 
 @Entity
 @Table(name = "key_exemptions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class KeyExemption {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "api_key_id")
+    @OneToOne
+    @JoinColumn(name = "api_key_id", nullable = false, unique = true)
     private ApiKey apiKey;
-
-    private String notes;
-
-    private Boolean unlimitedAccess;
 
     private Integer temporaryExtensionLimit;
 
-    private LocalDateTime validUntil;
-
-    public KeyExemption() {}
+    @Column(nullable = false)
+    private Instant validUntil;
 }
