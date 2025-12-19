0c852d95f6fc72a8a2699d4d1295130a7a3e104e
bala-amypo <bala@amypo.in>
2025-12-19 10:15:18 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,20 +1,19 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.List;
 
 @Configuration
-public class OpenApiConfig {
+public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        new Server().url("https://9005.vs.amypo.ai")
-                ));
-        }
+                .info(new Info()
+                        .title("API Rate Limiter & Quota Manager API")
+                        .version("1.0")
+                        .description("API key management, quota plans, usage logging, rate limit enforcement, and exemptions"));
+    }
 }
