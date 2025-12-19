de40c861a4fbe852fc6bdb7940963ccc35375ee3
bala-amypo <bala@amypo.in>
2025-12-19 05:16:26 +0000

    User commit

--- src/main/java/com/example/demo/repository/ApiKeyRepository.java
+++ src/main/java/com/example/demo/repository/ApiKeyRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.ApiKey;
 
 public interface ApiKeyRepository extends JpaRepository<ApiKey, Long> {
 
-    ApiKey findByKeyValue(String keyValue);
+    Optional<ApiKey> findByKeyValue(String keyValue);
 }
--- src/main/java/com/example/demo/repository/ApiUsageLogRepository.java
+++ src/main/java/com/example/demo/repository/ApiUsageLogRepository.java
@@ -2,26 +2,32 @@ package com.example.demo.repository;
 
 import java.time.Instant;
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import com.example.demo.entity.ApiUsageLog;
 
 public interface ApiUsageLogRepository extends JpaRepository<ApiUsageLog, Long> {
 
     List<ApiUsageLog> findByApiKey_Id(Long id);
 
-    @Query("SELECT u FROM ApiUsageLog u WHERE u.apiKey.id = :keyId AND u.timestamp BETWEEN :start AND :end")
+    @Query("""
+        SELECT l FROM ApiUsageLog l
+        WHERE l.apiKey.id = :keyId
+        AND l.timestamp BETWEEN :start AND :end
+    """)
     List<ApiUsageLog> findForKeyBetween(
             @Param("keyId") Long keyId,
             @Param("start") Instant start,
             @Param("end") Instant end
     );
 
-    @Query("SELECT COUNT(u) FROM ApiUsageLog u WHERE u.apiKey.id = :keyId AND u.timestamp BETWEEN :start AND :end")
-    long countForKeyBetween(
+    @Query("""
+        SELECT COUNT(l) FROM ApiUsageLog l
+        WHERE l.apiKey.id = :keyId
+        AND l.timestamp BETWEEN :start AND :end
+    """)
+    int countForKeyBetween(
             @Param("keyId") Long keyId,
             @Param("start") Instant start,
             @Param("end") Instant end
