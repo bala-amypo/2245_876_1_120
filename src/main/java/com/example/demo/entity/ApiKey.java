b59bc030b352532e69531b9b0a91f874aad805e2
bala-amypo <bala@amypo.in>
2025-12-21 12:13:55 +0000

    User commit

--- src/main/java/com/example/demo/entity/ApiKey.java
+++ src/main/java/com/example/demo/entity/ApiKey.java
@@ -27,5 +27,59 @@ public class ApiKey {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    // getters & setters
+    public ApiKey() {}
+
+    /* ================= GETTERS & SETTERS ================= */
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getKeyValue() {
+        return keyValue;
+    }
+
+    public void setKeyValue(String keyValue) {
+        this.keyValue = keyValue;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public QuotaPlan getPlan() {
+        return plan;
+    }
+
+    public void setPlan(QuotaPlan plan) {
+        this.plan = plan;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
